7db671da5f9166b4f0b06ea65b2a5860aa49c9b1
Christopher Eby <kreed@kreed.org>
2017-07-16 08:53:13 -0500

    import multiple tpits and output most recent version of project

--- export.sh
+++ export.sh
@@ -3,7 +3,11 @@ sqlite3 tpit.db <<!
 .headers on
 .mode csv
 .output out.csv
-select * from tpit ORDER BY CASE WHEN sheet_status='Complete' THEN 0 WHEN sheet_status='Future' THEN 1 WHEN sheet_status='Cancelled' THEN 2 END, projected_in_service;
+select t1.*
+FROM tpit t1 LEFT OUTER JOIN tpit t2
+ON t1.id = t2.id AND t1.sheet_date < t2.sheet_date
+WHERE t2.id IS NULL AND t1.id != ""
+ORDER BY CASE WHEN t1.sheet_status='Complete' THEN 0 WHEN t1.sheet_status='Future' THEN 1 WHEN t1.sheet_status='Cancelled' THEN 2 END, t1.projected_in_service;
 !
 echo -n 'var tpit_projects = ' > tpit.js
 ./node_modules/.bin/csvtojson out.csv >> tpit.js
